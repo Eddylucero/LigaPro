a64f3c45185eb7886723e8c890c63b4a73a35ccd
Eddylucero <luceroeddy8@gmail.com>
2025-06-04 18:04:11 -0500

    Programacion de las urls de Liga

--- Aplicaciones/Liga/urls.py
+++ Aplicaciones/Liga/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicioLiga, name='inicioLiga'),
+    path('', views.listarLigas, name='listarLigas'),  
+    path('nuevaLiga/', views.nuevaLiga, name='nuevaLiga'),  
+    path('guardarLiga/', views.guardarLiga, name='guardarLiga'),  
+    path('eliminarLiga/<int:id>/', views.eliminarLiga, name='eliminarLiga'),  
+    path('editarLiga/<int:id>/', views.editarLiga, name='editarLiga'),  
+    path('procesarEdicionLiga/<int:id>/', views.procesarEdicionLiga, name='procesarEdicionLiga'),  
 ]
