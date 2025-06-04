afb4604562e3812a1237487eb4a505fb9818305b
Eddylucero <luceroeddy8@gmail.com>
2025-06-04 17:03:31 -0500

    Edicion en la urls general

--- LigaPro/urls.py
+++ LigaPro/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Aplicaciones.Liga.urls')),
+    path('Equipo/', include('Aplicaciones.Equipo.urls')),
+
 ]
