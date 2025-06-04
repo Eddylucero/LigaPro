086fbec63ea822ebf08acc7e9ff9e808d108501c
Eddylucero <luceroeddy8@gmail.com>
2025-06-04 11:51:23 -0500

    Creacion de models de Liga y ediccion en app

--- Aplicaciones/Liga/apps.py
+++ Aplicaciones/Liga/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LigaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Liga'
+    name = 'Aplicaciones.Liga'
--- Aplicaciones/Liga/models.py
+++ Aplicaciones/Liga/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Liga(models.Model):
+    id = models.AutoField(primary_key=True)  
+    nombre = models.CharField(max_length=100, unique=True)  
+    país = models.CharField(max_length=50)  
+    temporada_actual = models.CharField(max_length=20)  
+    logo = models.FileField(upload_to='ligas', null=True, blank=True)  
+    reglamento = models.FileField(upload_to='ligas', null=True, blank=True)  
+
+    def __str__(self):
+        return self.nombre
