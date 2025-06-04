2fa2b0fe1e76e330247d33a051038052f650bde9
Eddylucero <luceroeddy8@gmail.com>
2025-06-04 11:53:58 -0500

    Creacion de models y configuracion de app

--- Aplicaciones/Equipo/models.py
+++ Aplicaciones/Equipo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Aplicaciones.Liga.models import Liga
 
 # Create your models here.
+class Equipo(models.Model):
+    id = models.AutoField(primary_key=True)  
+    nombre = models.CharField(max_length=100, unique=True)  
+    liga = models.ForeignKey(Liga, on_delete=models.CASCADE, related_name="equipos") 
+    ciudad = models.CharField(max_length=50)  
+    escudo = models.ImageField(upload_to='equipos', null=True, blank=True)  
+    ficha_tecnica = models.FileField(upload_to='equipos', null=True, blank=True)  
+    def __str__(self):
+        return f"{self.nombre} ({self.liga.nombre})"
+
